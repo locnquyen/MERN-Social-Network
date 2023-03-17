a1c9f23e5883b7bb02ef08b50df05d7f83159d6e
locnquyen <70251057+locnquyen@users.noreply.github.com>
2023-03-17 12:14:05 +0700

    finish crud user and fl unfl

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -15,7 +15,7 @@ export const updateUser = async (req, res) => {
         }
 
         try {
-            const user = await User.findByIdAndUpdate(req.params.id, 
+            const user = await User.findByIdAndUpdate(req.params.id,
                 req.body,
             );
             res.status(200).json("Account has been updated");
@@ -29,16 +29,70 @@ export const updateUser = async (req, res) => {
     }
 }
 
-export const deleteUser = (req, res) => {
+export const deleteUser = async (req, res) => {
+
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can delete only your account!");
+    }
 
 }
-export const getUser = (req, res) => {
 
+export const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        console.log("user", user)
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 }
-export const followUser = (req, res) => {
 
+
+export const followUser = async (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            //current user
+            const user = await User.findById(req.params.id);
+            //friend user
+            const currentUser = await User.findById(req.body.userId);
+
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ followers: req.body.userId });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
+                res.status(200).json("user has been followed");
+            }
+        } catch (err) {
+
+        }
+    }
 }
 
-export const unFollowUser = (req, res) => {
+export const unFollowUser = async(req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+          const user = await User.findById(req.params.id);
+          const currentUser = await User.findById(req.body.userId);
 
+          if (user.followers.includes(req.body.userId)) {
+            await user.updateOne({ $pull: { followers: req.body.userId } });
+            await currentUser.updateOne({ $pull: { followings: req.params.id } });
+            res.status(200).json("user has been unfollowed");
+
+          } else {
+            res.status(403).json("you don't follow this user");
+          }
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } else {
+        res.status(403).json("you cant unfollow yourself");
+      }
 }
